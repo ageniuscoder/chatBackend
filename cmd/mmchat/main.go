ef37d9ae2d11fc6af3988938734f698c505fb1ea
ageniuscoder <23bcs059@nith.ac.in>
2025-09-05 00:29:40 +0530

    main.go updated and config loaded and database connection is checked

--- cmd/mmchat/main.go
+++ cmd/mmchat/main.go
@@ -1,1 +1,39 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"log/slog"
+
+	"github.com/ageniuscoder/mmchat/backend/internal/config"
+	"github.com/ageniuscoder/mmchat/backend/internal/storage/sqlite"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	fmt.Println("Entry point of MmChat")
+	migrate := flag.Bool("migrate", false, "run migrations and exits")
+	flag.Parse()
+	//config part
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error Loading Env file: %v", err)
+	}
+	cfg := config.MustLoad()
+
+	//database handling
+	conn, err := sqlite.New(cfg.SQLITEDsn)
+	if err != nil {
+		log.Fatalf("Error loading to database: %v", err)
+	}
+	defer conn.Db.Close()
+
+	if *migrate {
+		if err := conn.Migrate(); err != nil {
+			log.Fatalf("Migration failed %v", err)
+		}
+		slog.Info("Migration Completed")
+		return
+	}
+}
