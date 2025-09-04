ef811f8a665151046b885e0541f97aa10c71ceaa
ageniuscoder <23bcs059@nith.ac.in>
2025-09-05 00:28:33 +0530

    added config.go

---
+++ internal/config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"log"
+	"os"
+	"strconv"
+)
+
+type Config struct {
+	Addr      string
+	JWTSecret string
+	JWTTTLMin int
+	SQLITEDsn string
+	OTPDigits int
+	OTPTTLSec int
+}
+
+func getenv(key, def string) string {
+	val := os.Getenv(key)
+	if val != "" {
+		return val
+	}
+	return def
+}
+
+func MustLoad() Config {
+	jwtttl, _ := strconv.Atoi(getenv("JWT_TTL_MIN", "1440"))
+	otpdigit, _ := strconv.Atoi(getenv("OTP_DIGITS", "6"))
+	otpttl, _ := strconv.Atoi(getenv("OTP_TTL_SEC", "300"))
+
+	cfg := Config{
+		Addr:      getenv("HTTP_ADDR", ":8080"),
+		JWTSecret: getenv("JWT_SECRET", "mangal"),
+		JWTTTLMin: jwtttl,
+		SQLITEDsn: getenv("SQLITE_DSN", "file:chat.db?_pragma=foreign_keys(ON)"),
+		OTPDigits: otpdigit,
+		OTPTTLSec: otpttl,
+	}
+	log.Printf("config Loaded %+v", cfg)
+	return cfg
+}
