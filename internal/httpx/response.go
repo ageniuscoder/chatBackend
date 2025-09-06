d8c18bdd7c6117e0e24016c75c700073a89ae0fd
ageniuscoder <23bcs059@nith.ac.in>
2025-09-06 16:13:56 +0530

    some fixes for validating

--- internal/httpx/response.go
+++ internal/httpx/response.go
@@ -6,6 +6,6 @@ func OK(c *gin.Context, v any) {
 	c.JSON(200, v)
 }
 
-func Err(c *gin.Context, code int, msg string) {
+func Err(c *gin.Context, code int, msg any) {
 	c.JSON(code, gin.H{"error": msg})
 }
